16c0ddb8abcb83bf5cf8a9c6a0f1b21ce10fc14d
Akash Mane <78850231+akashmane3938@users.noreply.github.com>
2022-09-21 04:32:24 -0700

    Take 3 inputs
    Write a program and take 3 inputs a,b,c

---
+++ uc1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+read -p "Enter the 1st number:" a
+read -p "Enter the 2nd number:" b
+read -p "Enter the 3rd number:" c
+
+echo "Value of A is- $a"
+echo "Value of B is- $b"
+echo "Value of c is- $c"
